3c82c8d2ebbd5e228b5e0c1c7aa2e7a0421f6118
Jordan Liekweg <jordanliekweg@ool-ad0203c5.dyn.optonline.net>
2018-04-11 12:25:59 -0400

    database

---
+++ ewokese-app/databse/seed.sql
@@ -0,0 +1,13 @@
+\c ewokese_app
+
+INSERT INTO user (username, password, nickname)
+VALUES
+('jordan', 'dog', 'lickway'),
+('tara', 'cat', 'tarbear'),
+('m.e.', 'bird', 'me');
+
+INSERT INTO conversation (name, user_array)
+VALUES
+('jordan', 'dog', 'lickway'),
+('tara', 'cat', 'tarbear'),
+('m.e.', 'bird', 'me');
