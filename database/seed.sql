cc002f0920f3d1d6ff5dad5a55d740e30c133a3d
Tara Fenton <tarafenton@Taras-MBP.fios-router.home>
2018-04-11 19:32:06 -0400

    update schema and seed, find userById

--- database/seed.sql
+++ database/seed.sql
@@ -6,12 +6,21 @@ VALUES
 ('tara', 'cat', 'tarbear'),
 ('m.e.', 'birds', 'me');
 
+INSERT INTO conversations (name, user_array)
+VALUES
+('ewokese', '{1, 2, 3}'),
+('coding', '{1, 2}'),
+('homework', '{2, 3}');
 
--- INSERT INTO messages (sender_id, conversation_id)
--- VALUES
--- (1, 2),
--- (2, 3),
--- (3, 1);
+INSERT INTO messages (sender_id, conversation_id, message)
+VALUES
+(1, 1, 'jordan message in ewokese'),
+(2, 1, 'tara message in ewokese'),
+(3, 1, 'm.e. message in ewokese'),
+(1, 2, 'jordan message in coding'),
+(2, 2, 'tara message in coding'),
+(2, 3, 'tara message in homework'),
+(3, 3, 'm.e. message in homework');
 --
 -- INSERT INTO user_conversations (user_id, conversation_id)
 -- VALUES
@@ -20,8 +29,3 @@ VALUES
 -- (3, 3);
 
 -- doesnt like the brackets
--- INSERT INTO conversations (name, user_array)
--- VALUES
--- ('computers', [1, 2]),
--- ('coding', [2, 3]),
--- ('jordan', [1, 2, 3]);
