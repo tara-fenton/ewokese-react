aa8549269437e1b6d9b90e2d0c0281901fcf1459
ME Hoagland <mehoagland@ool-ad02014d.dyn.optonline.net>
2018-04-12 20:12:51 -0400

    showing one users info will need to use ${id} later

--- ewokese-app/src/Profile/index.js
+++ ewokese-app/src/Profile/index.js
@@ -15,9 +15,12 @@ class Profile extends Component {
   }
   // Fetch all messages from API endpoint and put them in state.
   fetchUser() {
-    fetch("http://localhost:3000/api/users")
+    fetch("http://localhost:3000/api/user/1")
+    // we need to put ${id} where 1 is so when user signs in it will take them to their user profile
+
       .then(response => response.json())
       .then(usersAPIResponse => {
+        console.log(22, usersAPIResponse)
 
         this.setState({
           userData: usersAPIResponse,
@@ -31,8 +34,20 @@ class Profile extends Component {
     return (
       <div>
         {this.state.userLoaded ? (
-          this.state.userData.map(user => <div> {user.user_name} </div>)
-        ) : (<p>Loading Users</p>)}
+          <div>
+          <div>
+            {this.state.userData.user_name}
+          </div>
+
+          <div>
+          {this.state.userData.nick_name}
+        </div>
+      </div>
+        ) : (
+            <p>
+              Loading messages
+            </p>
+          )}
       </div>
     )}
 }
