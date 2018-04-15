10a6fea499b31c00826c25b274ae8eff49246257
ME Hoagland <mehoagland@ool-ad02014d.dyn.optonline.net>
2018-04-15 12:32:04 -0400

    getting up to date with the master

--- ewokese-app/src/Messages/index.js
+++ ewokese-app/src/Messages/index.js
@@ -27,29 +27,27 @@ class Messages extends Component {
   //   }
 
   render() {
-    const messages = this.props.messages.map(message => {
-      return (<div className="noselect" key={message.id} id={message.id}>
-        <li class="message left appeared">
-                 <div class="avatar"></div>
-                   <div class="text_wrapper">
-                     <div class="text">
-                       {" "}
-                       {message.message}{" "}
-                     </div>
-                   </div>
-               </li>
+    let counter = 0;
+      const messages = this.props.messages.map(message => {
+      // const className = `${this.state.userId} ? 'message left appeared' : 'message right appeared'}`
+      const className = `${counter} ? 'message left appeared' : 'message right appeared'}`
 
-
-      </div>);
-    });
-    return <div>
-      <ul className='messages'>
-        <li className="message left appeared">
-          {messages}
-        </li>
-      </ul>
-
-    </div>;
-  }
+        return (
+            <div className="noselect" key={message.id} id={message.id}>
+              <li className={className}>
+                <div className="avatar"></div>
+                <div className="text_wrapper">
+                  <div className="text">
+                    {" "}
+                    {message.message}{" "}
+                  </div>
+                </div>
+              </li>
+            </div>
+          )
+          counter ? counter = 1 : counter = 0
+        })
+        return <div>{messages}</div>;
+    }
 }
 export default Messages;
