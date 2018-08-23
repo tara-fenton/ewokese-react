8a321373ab8cc97e98040231d3ce89612fc542e6
Tara Fenton <tarafenton@ool-ad0201c5.dyn.optonline.net>
2018-08-22 21:47:12 -0400

    db connection for heruko

--- database/db-connection.js
+++ database/db-connection.js
@@ -1,20 +1,38 @@
-const promise = require('bluebird');
-const monitor = require('pg-monitor');
+// const promise = require('bluebird');
+// const monitor = require('pg-monitor');
+//
+// promise.config({
+//   longStackTraces: true,
+// });
+//
+// const initOptions = {
+//   promiseLib: promise,
+// };
+//
+// monitor.attach(initOptions);
+//
+// const pgp = require('pg-promise')(initOptions);
+//
+// const connectionURL = 'postgres://localhost:5432/ewokese_app';
+//
+// const db = pgp(connectionURL);
+//
+// module.exports = db;
 
-promise.config({
-  longStackTraces: true,
-});
+const pgp = require('pg-promise')();
 
-const initOptions = {
-  promiseLib: promise,
-};
+let db;
 
-monitor.attach(initOptions);
-
-const pgp = require('pg-promise')(initOptions);
-
-const connectionURL = 'postgres://localhost:5432/ewokese_app';
-
-const db = pgp(connectionURL);
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+    // Fill in with your local database name
+    database: 'ewokese_app', 
+    port: 5432,
+    host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+    // Heroku will set this variable for you.
+    db = pgp(process.env.DATABASE_URL);
+}
 
 module.exports = db;
